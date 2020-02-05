b069d86cd9cd4b7185d17b4da4eeb9dca216f8ee
Justin O'Boyle <justin@justinoboyle.com>
2020-02-04 20:34:17 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# OpenData - web
+
+## Setup
+
+1. Clone this repository
+2. `npm install`
+3. `npm run dev`
+
+## I need help!
+
+* [Next.js Tutorial](https://nextjs.org/learn/basics/getting-started/first-page)
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+// Here's an example of what a sample page looks like.
+// See more: https://nextjs.org/learn/basics/getting-started/first-page
+
+export default () => (
+  <div>
+    <p>Hello DSC!</p>
+  </div>
+)
