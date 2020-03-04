625283c3b8dc682dc2668c43931c175ad4f1f1ec
Justin O'Boyle <justin@justinoboyle.com>
2020-03-03 22:13:57 -0500

    add google chart

--- pages/generalbudget.js
+++ pages/generalbudget.js
@@ -1,6 +1,7 @@
 // Here's an example of what a sample page looks like.
 // See more: https://nextjs.org/learn/basics/getting-started/first-page
-
+import { render } from "react-dom";
+import { Chart } from "react-google-charts";
 import Link from "next/link"
 
 export default () => (
@@ -18,7 +19,15 @@ export default () => (
           <div class="largeStat">100%</div>
           <p>Here's a statistic that is 100% of a statistic. There are lots of statistics, but this one is <em>very</em> important.</p>
         </div>
-        <div class="accent">a</div>
+        <div class="accent"><div className={"my-pretty-chart-container"}>
+        <Chart
+          chartType="ScatterChart"
+          data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
+          width="100%"
+          height="400px"
+          legendToggle
+        />
+      </div></div>
       </section>
       <section class="section-generic">
         <div class="accent">a</div>
